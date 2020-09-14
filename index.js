5e5a5b091ac0e9ee4063f07ae1ecd3aecf220db6
Emma Fewer <emmafewer20@gmail.com>
2020-09-14 19:41:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function add(a + b) 
+function add(a, b) 
   return (1+80) // 81
+  
 function subtract(60 - 40) // 20
 function multiply(2 * 3.4) // 6.8 (there's that floating-point arithmetic again...)
 function divide(5.0 / 2.5) // 2
