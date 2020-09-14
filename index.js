03e8cee7fbdd4cea22f73369575e9f0b30e8484c
Emma Fewer <emmafewer20@gmail.com>
2020-09-14 19:52:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function add(a, b) {
-  return 1+80 // 81;
+function add(a,b) {
+  return 1+80;
 }
  
 
@@ -15,31 +15,3 @@ function divide(a, b) {
   return(5.0 / 2.5) // 2
 }
 
-var number = 10
- 
-function add5() {
-  number += 5
-}
- 
-function divideBy3() {
-  number /= 3
-}
- 
-divideBy3()
- 
-console.log(number) // 3.333333333335
- 
-add5()
- 
-console.log(number) // 8.333333333335
- 
-// reset number
-number = 10
- 
-add5()
- 
-console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
